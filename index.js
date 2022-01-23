49e6931b608fed7d2a0560474eb8e6573ad730f6
NathalyEspinal <espinalnathaly@gmail.com>
2022-01-22 22:18:17 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+
+main.remove();
+//document.body.append(element);//
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+
+newHeader.innerHTML = "YOUR-NAME is the champion"
+
+//element.append(h1);//
+
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,7 +1,8 @@
 describe("index.html", () => {
   describe("after index.js is processed", () => {
     it("no longer has DOM node 'main#main'", () => {
-      expect(document.querySelector('main'), "Make sure you remove the <main> with id 'main'").to.not.exist
+      expect(document.querySelector('main'), "Make sure you remove the <main> with id 'main'")
+      .to.not.exist
     });
 
     it("has a 'newHeader' variable that points to node 'h1#victory'", () => {
@@ -12,7 +13,7 @@ describe("index.html", () => {
       expect(newHeader.id, "Make sure you create an <h1> with id 'victory'").eql('victory')
     });
 
-    it("has a 'newHeader' variable that points to node 'h1#victory' with \"YOUR-NAME is the champion\" inside", () => {
+    it("has a 'newHeader' variable that points to node 'h1#victory' with \"YOUR-NAME is the     champion\" inside", () => {
       expect(newHeader.innerHTML, "Make sure you create an <h1> with id 'victory' with a sweet message in it").to.include("is the champion");
     });
 
